e3aeac00989c4775c2b2d2bb402b3f0e0a95c95c
Pushpa Agr <pushpaagr108@gmail.com>
2018-11-16 16:06:30 -0800

    wave 3

--- src/adagrams.js
+++ src/adagrams.js
@@ -70,6 +70,55 @@ const Adagrams = {
     return true;
   },
 
+  scoreWord(word){
+      const letterScores = {
+        "A": 1,
+        "E": 1,
+        "I": 1,
+        "O": 1,
+        "U": 1,
+        "L": 1,
+        "N": 1,
+        "R": 1,
+        "S": 1,
+        "T": 1,
+        "D": 2,
+        "G": 2,
+        "B": 3,
+        "C": 3,
+        "M": 3,
+        "P": 3,
+        "F": 4,
+        "H": 4,
+        "V": 4,
+        "W": 4,
+        "Y": 4,
+        "K": 5,
+        "J": 8,
+        "X": 8,
+        "Q": 10,
+        "Z": 10
+      }
+
+
+      let totalScore = 0;
+
+      let UpperCaseWord = word.toUpperCase();
+      console.log(totalScore);
+
+
+
+      for (let letter of UpperCaseWord) {
+        totalScore += letterScores[letter];
+      }
+
+      if (UpperCaseWord.length > 6) {
+        totalScore += 8;
+      }
+      console.log(totalScore);
+      return totalScore;
+
+    },
 
 
 
